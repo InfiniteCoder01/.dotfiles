8cc54197b5089895c6bf49de42e44c070ae6ede3
InfiniteCoder <nayka.0.lobach.01@gmail.com>
2024-01-26 14:21:07 +0300

    Update

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,7 @@
 cp ~/.config/mimeapps.list KDE
 cp ~/.config/plasma-org.kde.plasma.desktop-appletsrc KDE
+
+cp ~/.config/PrusaSlicer/PrusaSlicer.ini PrusaSlicer
+
+cp ~/.config/obs-studio/global.ini obs-studio
+cp ~/.config/obs-studio/basic/scenes/Streaming.json /mnt/D/Channel/Private/OBS/basic/scenes/
