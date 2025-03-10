e848f6bb5378469201d2993deb3ece84b89329c9
InfiniteCoder <info@infinitecoder.org>
2025-03-10 16:49:00 +0300

    Update

--- system/configuration.nix
+++ system/configuration.nix
@@ -54,7 +54,7 @@
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
